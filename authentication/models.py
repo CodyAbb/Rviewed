794f63959e75a13cb27690638f80ab5928ae12e5
Cody Abbott <cody.abbott16@gmail.com>
2020-05-12 12:07:18 +0100

    adjusted user class to use email field add JWT

--- authentication/models.py
+++ authentication/models.py
@@ -2,6 +2,9 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
+    USERNAME_FIELD = 'email'
+    email = models.EmailField(('email address'), unique=True) # changes email to unique and blank to false
+    REQUIRED_FIELDS = [] # removes email from REQUIRED_FIELDS
     #This extends the user model with extra fields
     bio = models.TextField(max_length=500, blank=True)
     location = models.CharField(blank = True, max_length = 120)
--- rviewed/settings.py
+++ rviewed/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from datetime import timedelta
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'authentication',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -73,6 +75,29 @@ WSGI_APPLICATION = 'rviewed.wsgi.application'
 
 #Extended user model
 AUTH_USER_MODEL = "authentication.CustomUser"
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),  
+}
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=14), #refresh token allowing 14 day sign in
+    'ROTATE_REFRESH_TOKENS': True, #changes token so user does not have to re-sign in 
+    'BLACKLIST_AFTER_ROTATION': False,
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+    'VERIFYING_KEY': None,
+    'AUTH_HEADER_TYPES': ('JWT',),
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+}
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
