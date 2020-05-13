ff5328f6ce568c0ca19fad1e579e43ca82f55c9e
Cody Abbott <cody.abbott16@gmail.com>
2020-05-13 11:14:55 +0100

    basic routing added for login and signup page

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,5 +1,18 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Login from "./Login";
+import SignUp from "./SignUp";
 
 export default function App() {
-  return <div>This should now be working!</div>;
+  return (
+    <div>
+      <main>
+        <Switch>
+          <Route exact path={"/login/"} component={Login} />
+          <Route exact path={"/signup/"} component={SignUp} />
+          <Route path={"/"} render={() => <div>Home again</div>} />
+        </Switch>
+      </main>
+    </div>
+  );
 }
